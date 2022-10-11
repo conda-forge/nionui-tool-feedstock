997403fc74d0c95567b6ddc79905e49974b3b75b
Chris Meyer <cmeyer1969@gmail.com>
2022-10-11 11:33:05 -0700

    Run build on osx-arm64.

--- recipe/build.sh
+++ recipe/build.sh
@@ -22,23 +22,17 @@ if [[ "$target_platform" == linux* ]]; then
   "${PYTHON}" -m pip install --no-deps --ignore-installed .
 fi
 
-if [[ "$target_platform" == "osx-64" ]]; then
-  # set
-  # pwd
-  # ls -R .
-  # cp ${RECIPE_DIR}/CMakeLists.txt launcher/CMakeLists.txt
+if [[ "$target_platform" == osx* ]]; then
   pushd launcher
-  # conda info
-  # conda list
-  # conda env list
-  # "${PYTHON}" --version
+  # Ensure the tools are run using the host Qt, not the target Qt.
+  export QT_HOST_PATH="$BUILD_PREFIX"
+  # see https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+  export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
   # default python is from the activate conda environment which is not the build environment; specify python to cmake
-  cmake CMakeLists.txt -DPython3_EXECUTABLE="$PYTHON" -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
+  cmake ${CMAKE_ARGS} CMakeLists.txt -DPython3_EXECUTABLE="$PYTHON" -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
   cmake --build . --config Release
-  # ls -R .
   mkdir -p build/Release
   mv build/*.app build/Release
   popd
-  # ls -R .
   "${PYTHON}" -m pip install --no-deps --ignore-installed .
 fi
